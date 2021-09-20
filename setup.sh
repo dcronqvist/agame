c012c0093a36373f5908ac728581a9d23483c479
Daniel Cronqvist <dancro99@gmail.com>
2021-09-20 02:12:48 +0200

    Fixed automatic download of GLFW binaries

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ cd tmp &&
 curl -sSL https://github.com/glfw/glfw/releases/download/3.3.4/glfw-3.3.4.bin.WIN64.zip > winglfw.zip &&
 unzip winglfw.zip &&
 rm winglfw.zip &&
+mkdir -p ../libs/win &&
 cp ./glfw-3.3.4.bin.WIN64/lib-vc2019/glfw3.dll ../libs/win/glfw3.dll &&
 
 cd .. &&
